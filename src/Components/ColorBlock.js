89c4292e79b9a8c1defdd113a7d36e4a4521cbc3
Laith Albusairi <laithalbusairi@gmail.com>
2022-10-12 18:44:37 -0700

    finshed reacte raindbow

---
+++ src/Components/ColorBlock.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ColorBlock(props) {
+  return (
+    <div
+      className="colorBlock"
+      style={{ 'backgroundColor': props.color }}
+    >
+      {props.color}
+    </div>
+  )
+}
+
+export default ColorBlock; 
